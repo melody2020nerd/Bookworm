bc61abdd0ac78be50e69b21bfec5cf03cfea6436
melody2020nerd <princessanoliefo21@gmail.com>
2022-05-26 06:35:31 +0100

    first complete website

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Bookly
+
+Bookly is an online library that provides books to users for free.
+
+## Installation
+
+
+
+
+## Usage
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ document.querySelector('#search-btn').onclick = ()  => {
     searchForm.classList.toggle('active');
 }
 
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+}
+
 window.onscroll = () =>{
 
 searchForm.classList.remove('active');
@@ -22,3 +32,9 @@ window.onload  = () =>{
         document.querySelector('.header.header-2').classList.remove('active');
     }
 }
+
+
+
+
+
+
